55be5c0666cbe23c1866aa27f004a57c9f6290e3
Sean Martin <sean@seanmartinstudio.com>
2022-01-11 15:28:13 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,35 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+//Global variable for toy object URL
+const toyObjectURL = 'http://localhost:3000/toys'
+
+//Initialize Function to envoke all other functions
+function init() {
+getToyObjects()
+rendertoyObjects()
+}
+init()
+
+//Render toy objects to DOM
+function rendertoyObjects(toys) {
+const toyCard = document.createElement('div')
+toyCard.id = `toy-${toys.id}`
+console.log(toyCard)
+}
+
+
+//Fetch toy objects from server
+function getToyObjects() {
+fetch(toyObjectURL)
+.then(res => res.json())
+.then(toys => {
+  toys.forEach(rendertoyObjects)
+})
+}
+
+
+//To Do: Continue building out toy div cards in renderToyObjects function,
+//next step is to deal with bug at line 31 and then create the class for each card.
